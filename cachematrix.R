9db115367073887e552e779c1f259485e8cbb523
Sawetr Suchit-rattanant <angpao43@gmail.com>
2024-02-09 13:00:49 +0700

    the final answer

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,36 @@
 ## functions do
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+##the function will return the list of functions that include set, get, setmean, and getmean
+makeVector <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
 }
 
 
+
 ## Write a short comment describing this function
+## This function will check if the mean from x is already start by checking it value 
+## if the value is null,then it will return the original value of x$getmean back
+## if the value is not null, then it will return the mean value
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setmean(m)
+        m
 }
